a4a313a6ff807ea9e821602ad9a4d78c85871b43
matsteele <matsteele@gmail.com>
2020-07-30 21:54:33 -0400

    built start of API call working in global scope

--- API_call.js
+++ API_call.js
@@ -1,48 +1,57 @@
 const fetch = require('node-fetch');
 
-
 class APICall {
-    constructor(HeadersAndData){
-        // console.log('HeadersAndData', HeadersAndData)
-        if(HeadersAndData.library)
-            this.APICall = HeadersAndData.APICall
-        else
-            {
-                this.HeadersAndData  = HeadersAndData
-                this.APICall = this._APICall
-            
-            }
+  constructor(InitiateAPI) {
+    if (InitiateAPI.libraryCall) {
+      this.APIClient = InitiateAPI.APIClient;
+      this.label = InitiateAPI.APILabel;
+
+      this.indicator = InitiateAPI.indicator;
+      this.period = InitiateAPI.indicator;
+    } else {
+      this.APICall = this._APICallDirect(InitiateAPI.HeadersAndData);
     }
+  }
 
-    _APICall(){
-        const HeadersAndData = this.HeadersAndData
-        let results = {}
-        fetch(HeadersAndData.url, {
-            method: 'POST', // or 'PUT'
-            headers: HeadersAndData.headers,
-            body: HeadersAndData.body,
-            })
-            .then((response) => response.json())
-            .then((data) => {
-            console.log('Success:', data, data.Results);
-            results = data.Results
-    
-            })
-            .catch((error) => {
-            console.error('Error:', error);
-    
-        });
-       return results
-    }
+  async populateValuesForDates(start, end, resolution) {
+    console.log('passing', this.ticker, resolution, start, end);
+    this.values = await this.APIClient.stockCandles(
+      this.ticker,
+      resolution,
+      start,
+      end,
+      {},
+      (error, data, response) => {
+        //   console.log('data', data)
+        this.values = data;
+      }
+    );
+    console.log('this.values', this.values);
+  }
 
-    APIDataCheck(){
-        //make sure that the format is as expected. 
-        // better as a unit test 
+  _APICallDirect() {
+    const HeadersAndData = this.HeadersAndData;
+    let results = {};
+    fetch(HeadersAndData.url, {
+      method: 'POST', // or 'PUT'
+      headers: HeadersAndData.headers,
+      body: HeadersAndData.body,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data, data.Results);
+        results = data.Results;
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    return results;
+  }
 
-    }
-
-
-
+  APIDataCheck() {
+    //make sure that the format is as expected.
+    // better as a unit test
+  }
 }
 
-module.exports = APICall
+module.exports = APICall;
